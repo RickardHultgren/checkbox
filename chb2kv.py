f6a15ba241fca558f134ce66f3f4a933b224db5c
Rickard Hultgren <sababa.sababa@gmail.com>
2018-09-14 04:13:36 +0000

    Update chb2kv.py

--- chb2kv.py
+++ chb2kv.py
@@ -1,4 +1,15 @@
-with open("in_file.txt","r") as fo: 
-    pat=re.compile(r'^([^\n]*?blocks are definitely lost in loss record.*?loss record)', re.S | re.M)
-    for i, block in pat.finditer(of.read()):
-         # deal with each block
+varbegin = False
+
+def regfunc(string):
+	with open("in_file.txt","r") as fo: 
+		pat=re.compile(r'^([^\n]*?blocks are definitely lost in loss record.*?loss record)', re.S | re.M)
+		for i, block in pat.finditer(of.read()):
+			# deal with each block
+
+if regstring == "begin":
+	varbegin = True
+	regfunc(regstring)
+elif regstring == "end":
+	varbegin = False
+
+vardic={name:value}
